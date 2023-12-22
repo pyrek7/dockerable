cf7321d3a56d94786dc0cfd0fdbc3abd4fef1c26
pawelfiderek <fiderek@gmail.com>
2023-12-22 10:33:04 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace dockerable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.WriteLine("Hello World!\n");
+                System.Threading.Thread.Sleep(1000);
+            }
+            
+        }
+    }
+}
